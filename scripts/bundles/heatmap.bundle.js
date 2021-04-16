628059123eb63acbd8a08254f0c574d675f12bbf
William <Williamaxb@gmail.com>
2021-04-16 10:04:36 +0200

    First draft of contact/about us page

--- scripts/bundles/heatmap.bundle.js
+++ scripts/bundles/heatmap.bundle.js
@@ -409,7 +409,7 @@ function initMap() {
     var stockholm = new window.google.maps.LatLng(59.3293235, 18.0685808);
     map = new window.google.maps.Map(window.document.getElementById('map'), {
         center: stockholm,
-        zoom: 13,
+        zoom: 11,
         mapTypeId: 'satellite'
     });
     var heatmap = new window.google.maps.visualization.HeatmapLayer({
