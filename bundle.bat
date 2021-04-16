2dd88c8975fe8cc6522f10b0f84b186dc2562db3
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-04-16 12:57:08 +0200

    Added description for bundling

--- bundle.bat
+++ bundle.bat
@@ -1,5 +1,6 @@
 @echo off
-rem deno run --unstable --allow-read --allow-net --allow-write bundle.ts
+rem Add all TypeScript files you want to be bundled here
 deno bundle scripts/heatmap.ts scripts/bundles/heatmap.bundle.js
 
+rem Do not touch this line plz
 deno run --unstable --allow-read --allow-net --allow-write process-bundles.ts
