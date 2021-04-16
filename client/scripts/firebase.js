2d4ab90c769fb971a5ab47e02dda12c9f96a9432
William <Williamaxb@gmail.com>
2021-04-16 15:14:14 +0200

    Start of Firebase implementation

---
+++ client/scripts/firebase.js
@@ -0,0 +1,14 @@
+
+export function initFirebase() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyBI1xMKhZzzEbOeq5NwKTu3bNJQzAQqM6U",
+    authDomain: "airdash-eb4f7.firebaseapp.com",
+    projectId: "airdash-eb4f7",
+    storageBucket: "airdash-eb4f7.appspot.com",
+    messagingSenderId: "115572624463",
+    appId: "1:115572624463:web:9cbeec099147ee6b44ee3a",
+    measurementId: "G-KTT3J57LNB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
