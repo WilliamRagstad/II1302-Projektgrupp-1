4c776957a7b0fdc8ab0a4766ee3343950cd16dac
William <Williamaxb@gmail.com>
2021-04-17 16:13:17 +0200

    Added first draft of Firebase/Firestore functionality.

--- client/scripts/firebase.js
+++ client/scripts/firebase.js
@@ -1,5 +1,3 @@
-
-export function initFirebase() {
   var firebaseConfig = {
     apiKey: "AIzaSyBI1xMKhZzzEbOeq5NwKTu3bNJQzAQqM6U",
     authDomain: "airdash-eb4f7.firebaseapp.com",
@@ -9,6 +7,53 @@ export function initFirebase() {
     appId: "1:115572624463:web:9cbeec099147ee6b44ee3a",
     measurementId: "G-KTT3J57LNB"
   };
-  // Initialize Firebase
+  // Initialize Firebase and firestore
   firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+
+class Coordinate {
+  constructor (lat, long) {
+    this.lat = lat;
+    this.long = long;
+  }
+  toString() {
+    return "new window.google.maps.LatLng("+this.lat+","+ this.long+")"
+  }
+};
+
+//Converts Coordinate from JSON to an object and back.
+var coordinateConverter = {
+  toFirestore: function(coordinate) {
+    return {
+      lat: coordinate.lat,
+      long: coordinate.long
+    };
+  },
+  fromFirestore: function(data){
+    return new Coordinate(data.lat, data.long, data.weight);
+  }
+};
+
+//Retrieves all coordinates from Firestore in object form
+function getCoordinates() {
+  const data = [];
+  db.collection("testdata").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          data.push(coordinateConverter.fromFirestore(doc.data()))
+        });
+      });
+  return data;
+}
+
+//Adds a list of coordinates to the firestore. Intended only for testing purposes.
+function setCoordinates(list) {
+  list.forEach(coord =>
+    db.collection("testdata").add(coord)
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    }))
 }
