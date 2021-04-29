60d6ac1bfb7525caf09597b81057a6fafc2611d2
Kristoffer <kribur@kth.se>
2021-04-29 08:04:19 -0400

    faktiska koden för mmap allokering. ples no touchy touchy

---
+++ Docs/Hardware/Drivers/mmap.c
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcnt1.h>
+#include <sys/mman.h>
+#include <uinstd.h>
+#include <stdint.h>
+
+//#include <Viktor.h>
+//#include <Basel.h>
+
+int main() {
+
+	/*
+	strukturen:
+	* start läser in värdena
+	*
+	* formaterar
+	*
+	* etablerar en binding till servern
+	*
+	* skickar videon
+	*
+	* skickar resten datan
+	*
+	* gör att videon nu skriver vidare tills den får slut på disk minne
+	* (i test fallet kör vi på en hel minuts lagring)
+	//*/
+
+	int *mmap;
+
+	printf("\nmmap funtionality test: %d", mmap);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
