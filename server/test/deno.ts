e01a509a94ea8ede9af40aec322fcc2bcd988485
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-04-29 19:08:51 +0200

    Redundant test removal suggestion

--- server/test/deno.ts
+++ server/test/deno.ts
@@ -1,5 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.94.0/testing/asserts.ts";
 
+/**
+ *? Is this test really necessary?
+ * Deno probably has their own unit tests
+ * before shipping the functionality for
+ * testing into production.
+ */
+
 export function testDeno() {
 	//Tests the Deno.test function
 	Deno.test({
