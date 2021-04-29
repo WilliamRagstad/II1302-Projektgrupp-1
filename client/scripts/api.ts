3552d9fad672f0565e9c38f59a6fc9aef5f223ac
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-04-29 15:15:43 +0200

    Added status check for API calls

--- client/scripts/api.ts
+++ client/scripts/api.ts
@@ -1,6 +1,7 @@
 export const API = {
 	Get: async function (endpoint: string) {
 		const response = await fetch(endpoint.startsWith('/') ? endpoint : `/${endpoint}`);
+		if (response.status !== 200) return false;
 		try {
 			return await response.json();
 		} catch (error) {
