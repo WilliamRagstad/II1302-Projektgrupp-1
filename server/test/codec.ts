caa36cb208578294fd2a0959510ecf126cee9699
William <Williamaxb@gmail.com>
2021-04-30 13:37:58 +0200

    Added Unit tests for Codec.ts

---
+++ server/test/codec.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "https://deno.land/std@0.94.0/testing/asserts.ts";
+import { Codec } from "../lib/codec.ts";
+
+export function testCodec(){
+  //Testing the Codec Info function.
+  Deno.test({
+		name: "Codec Info Test",
+		async fn() {
+      var data = Codec.Info("3E0C2CA38FB4{ÈA#59.334,18.063");
+      assertEquals(data, { Succeeded: true, Result: { MAC: "3E0C2CA38FB4", Accelerometer: 1949, GPS: { lat: 59.334, long: 18.063}} })
+		},
+	});
+
+  //Testing the Codec Video function.
+  Deno.test({
+    name: "Codec Video Test",
+    async fn() {
+      var data = Codec.Video("3E0C2CA38FB4_*@^a1<+?3");
+      assertEquals(data, { Succeeded: true, Result: { MAC: "3E0C2CA38FB4", Data: "_*@^a1<+?3" } })
+    },
+  });
+}
--- server/test/index.ts
+++ server/test/index.ts
@@ -2,8 +2,10 @@ import { testDeno } from "./deno.ts";
 import { testHeatmap } from "./heatmap.ts";
 import { testFirebase } from "./firebase.ts";
 import { testEndpoints } from "./endpoints.ts";
+import { testCodec } from "./codec.ts";
 
 testDeno();
 testHeatmap();
 testFirebase();
 testEndpoints();
+testCodec();
