587e230ca2ea1514c6ecaac4a62aee2c7158494b
Koperofag <baselm@kth.se>
2021-05-03 11:27:19 +0200

    Fixed Compiling errors

--- Docs/Hardware/Drivers/rpi.h
+++ Docs/Hardware/Drivers/rpi.h
@@ -7,17 +7,16 @@
 
 #define BCM2708_PERI_BASE 0x20000000
 #define GPIO_BASE (BCM2708_PERI_BASE + 0x200000)
-#define BLOCKSIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
 
 
-#define INP_GPIO(g) *(gpio+((g)/10))&=~(7<<(((g)%10)*3))
-#define OUT_GPIO(g) *(gpio+((g)/10)) |= (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(a,g) *(gpio+(((g)/10)))|=(((a)<=3?(a)+4:(a)==a?3:2)<<(((g)%10)*3))
-#define GPIO_SET *(gpio+7)
-#define GPIO_CLR *(gpio+10)
-#define GPIO_READ(g) (*(gpio+13)&(1<<g))
-#define GPIO_PULL *(gpio+37)
-
+#define INP_GPIO(g) *(gpio.addr+((g)/10))&=~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio.addr+((g)/10)) |= (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(a,g) *(gpio.addr+(((g)/10)))|=(((a)<=3?(a)+4:(a)==a?3:2)<<(((g)%10)*3))
+#define GPIO_SET *(gpio.addr + 7)
+#define GPIO_CLR *(gpio.addr + 10)
+#define GPIO_READ(g) (*(gpio.addr+13)&(1<<g))
+#define GPIO_PULL *(gpio.addr+37)
 
 
 struct bcm2835_peripheral {
@@ -27,5 +26,11 @@ struct bcm2835_peripheral {
 	volatile unsigned int *addr;
 };
 
+
+
+
 struct bcm2835_peripheral gpio = {GPIO_BASE};
 extern struct bcm2835_peripheral gpio;
+
+int map_peripheral(struct bcm2835_peripheral *p);
+void unmap_peripheral(struct bcm2835_peripheral *p);
