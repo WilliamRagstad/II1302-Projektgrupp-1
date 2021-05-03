c1facc003d28102baaebb07b26cce30e4552f43c
Ludvig Lindén <ludvig978@gmail.com>
2021-05-03 11:43:14 +0200

    Removed unused async declarations

--- server/test/codec.ts
+++ server/test/codec.ts
@@ -5,7 +5,7 @@ export function testCodec(){
   //Testing the Codec Info function.
   Deno.test({
 		name: "Codec Info Test",
-		async fn() {
+		fn() {
       var data = Codec.Info("3E0C2CA38FB4{ÈA#59.334,18.063");
       assertEquals(data, { Succeeded: true, Result: { MAC: "3E0C2CA38FB4", Accelerometer: 1949, GPS: { lat: 59.334, long: 18.063}} })
 		},
@@ -14,7 +14,7 @@ export function testCodec(){
   //Testing the Codec Video function.
   Deno.test({
     name: "Codec Video Test",
-    async fn() {
+    fn() {
       var data = Codec.Video("3E0C2CA38FB4_*@^a1<+?3");
       assertEquals(data, { Succeeded: true, Result: { MAC: "3E0C2CA38FB4", Data: "_*@^a1<+?3" } })
     },
