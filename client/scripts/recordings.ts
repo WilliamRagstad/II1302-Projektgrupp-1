fd2ab9da28133f7ec51aec4e9ef31e9e89b59237
astroxq55 <52320146+astroxq55@users.noreply.github.com>
2021-05-05 16:28:30 +0200

    remove dummy code

--- client/scripts/recordings.ts
+++ client/scripts/recordings.ts
@@ -6,17 +6,6 @@ declare global {
 	}
 }
 
-/*
-let videos = [
-	{id: "11:12:11:11", date: "12/11/2019", url: "blaha8"},
-	{id: "11:11:11:18", date: "12/11/2010", url: "blaha9"},
-	{id: "00:11:11:11", date: "10/11/2012", url: "blaha10"},
-	{id: "11:23:11:16", date: "09/11/2012", url: "blaha11"},
-	{id: "22:11:11:11", date: "08/11/2012", url: "blaha12"},
-	{id: "33:11:11:11", date: "07/11/2012", url: "blaha13"},
-	{id: "11:00:11:10", date: "06/11/2012", url: "blaha14"}
-];
-*/
 async function generateTableContent(){
 	const URL = await API.Get('video/88:88:88:88');
 	var videos:any[] = [];
