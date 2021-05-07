6cc4dcaabcef02fad34dd62732b1e07c7bd2647a
flumster <vsjob@kth.se>
2021-05-07 12:59:22 +0200

    added functionality

--- Docs/Hardware/Drivers/mpu6050.py
+++ Docs/Hardware/Drivers/mpu6050.py
@@ -33,6 +33,7 @@ def MPU_Init():
 	#write to interrupt enable config register
 	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
+	#write to accelerometer configuration register, allow 16g detection
 	bus.write_byte_data(Device_Address, ACCEL_CONFIG, 24)
 
 def read_raw_data(addr):
@@ -58,6 +59,10 @@ MPU_Init()
 
 print("reading acc")
 
+file1 = open("datafile.txt", "w")
+s1 = "Ax = "
+s2 = "Ay = "
+s3 = "Az = "
 while True:
 
 	#Read acc raw vals
@@ -80,7 +85,33 @@ while True:
 	Gz = gyro_z/131.0
 
 
+
+
+#	acc_data = ["Ax = %.2f g", %Ax, "Ay = %.2f g" %Ay, "Az = %.2f g" %Az]
+
+#	file1 = open("datafile.txt", "w")
+#	file1.write(s1)
+#	file1.write('{}\n'.format(Ax))
+#	file1.write(s2)
+#	file1.write('{}\n'.format(Ay))
+#	file1.write(s3)
+#	file1.write('{}\n'.format(Az))
+
 #	print("Gx=%.2f" %Gx, "Gy=%.2f" %Gy,"Gz=%.2f" %Gz, "Ax=%.2f g" %Ax, "Ay=%.2f g" %Ay, "Az=%.2f g" %Az)
-	print("Ax = %.2f g" %Ax, "Ay = %.2f g" %Ay, "Az = %.2f g" %Az)
+#	print("Ax = %.2f g" %Ax, "Ay = %.2f g" %Ay, "Az = %.2f g" %Az)
+
+	if(Az > 2.0 ):
+		print("crash")
+		print("Az = %.2f g" %Az)
+		file1.write(s3)
+		file1.write('{}\n'.format(Az))
+
+	if(Az < -2.0):
+		print("crash")
+		print("Az = %.2f g" %Az)
+		file1.write(s3)
+		file1.write('{}\n'.format(Az))
+
 	sleep(1)
 	#sleep(0.02)
+f.close()
