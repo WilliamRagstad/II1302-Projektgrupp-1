68a7b76cc91d1fca30d8bfcd4b54c1e1fd0fa1ca
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-05-10 16:02:32 +0200

    Remove unused imports

--- server/test/gui/heatmap-gui.ts
+++ server/test/gui/heatmap-gui.ts
@@ -1,20 +1,19 @@
 import { HeadlessBrowser } from "https://deno.land/x/sinco@v1.1.1/mod.ts";
-import { assertEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
 export function testHeatmapGui() {
-    //Tests navigation to index page.
-    Deno.test({
-        name: "Test index navigation",
-        async fn() {
-            //Setup
-            const Sinco = new HeadlessBrowser();
-            await Sinco.build();
-            await Sinco.goTo("https://airdash.herokuapp.com/heatmap");
+	//Tests navigation to index page.
+	Deno.test({
+		name: "Test index navigation",
+		async fn() {
+			//Setup
+			const Sinco = new HeadlessBrowser();
+			await Sinco.build();
+			await Sinco.goTo("https://airdash.herokuapp.com/heatmap");
 
-            await Sinco.click('a[href="/"]');
-            await Sinco.waitForPageChange();
-            await Sinco.assertUrlIs("https://airdash.herokuapp.com/");
-            await Sinco.done();
-        }
-    })
+			await Sinco.click('a[href="/"]');
+			await Sinco.waitForPageChange();
+			await Sinco.assertUrlIs("https://airdash.herokuapp.com/");
+			await Sinco.done();
+		}
+	})
 }
