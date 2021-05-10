bfdad3ceb1189355b2f8aeedca6127bf33e40fc4
Ludvig Lindén <ludvig978@gmail.com>
2021-05-10 14:46:35 +0200

    Added some simple gui tests

--- server/test/index.ts
+++ server/test/index.ts
@@ -2,8 +2,10 @@ import { testHeatmap } from "./heatmap.ts";
 import { testFirebase } from "./firebase.ts";
 import { testEndpoints } from "./endpoints.ts";
 import { testCodec } from "./codec.ts";
+import { testGui } from "./gui/index.ts";
 
 testHeatmap();
 testFirebase();
 testEndpoints();
 testCodec();
+testGui();
