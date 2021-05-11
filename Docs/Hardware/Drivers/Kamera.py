8eaf34d1a094c7916683bd7ca62023321c74b71c
Kristoffer <kribur@kth.se>
2021-05-11 08:09:41 -0400

    changed the kamera file to be cleaned from humor aswell. verified functionality

--- Docs/Hardware/Drivers/Kamera.py
+++ Docs/Hardware/Drivers/Kamera.py
@@ -1,14 +1,14 @@
 from picamera import PiCamera
 from time import sleep
-ThotCam = PiCamera()
-ThotCam.rotation = (69 * 90)
+Cam = PiCamera()
+Cam.rotation = (69 * 90)
 
-print("OnlyFans pic for my KAWAiI subs!!")
-ThotCam.start_preview()
-ThotCam.start_recording("/home/pi/Desktop/Hot_tub_Review.h264")
+print("picture inprogress. please wait")
+Cam.start_preview()
+Cam.start_recording("/home/pi/Documents/Docs/Hardware/Drivers/test.h264")
 sleep(5)
-ThotCam.stop_recording()
+Cam.stop_recording()
 sleep(0.1)
-ThotCam.capture("/home/pi/Desktop/Bathwater.jpg")
-ThotCam.stop_preview()
+Cam.capture("/home/pi/Documents/Docs/Hardware/Drivers/pic.jpg")
+Cam.stop_preview()
 
--- Docs/Hardware/Drivers/post.py
+++ Docs/Hardware/Drivers/post.py
@@ -1,20 +1,24 @@
 import requests
+import json
 
-#url ='https://airdash.herokuapp.com/info'
 testval = {
 	'MAC': '3E0C2CA38FB4',
 	'Accelerometer' : '12',
-	'GPS' : {
+	'GPS' :json.dumps( {
 		'lat': '-31.212',
 		'long': '5.6752'
-	}
+	})
 }
 
-headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 FireFox/24.0', 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip', 'DNT' : '1', #Do not Track Request Headers
-			'Connection' : 'close'}
+headers = {'content-type':'application/raw','content-length':'128', 
+	"x-MAC": '3E0C2CA38FB4', "x-Accelerometer" : '12',
+	"GPS" :json.dumps( {"lat": '-31.212',"long": '5.6752'})
+}
 
-x = requests.post('http://airdash.herokuapp.com/info', headers = headers, data = testval)
+x = requests.post('http://airdash.herokuapp.com/info',  
+headers = headers, 
+data = testval)
 
 print(x.text)
 print(x.status_code)
+  
