2efe0767e01de1289fdf20e30cd8e057b42948b1
Ludvig Lindén <ludvig978@gmail.com>
2021-05-12 13:21:46 +0200

    Moved some tests and renamed folder

--- server/test/daily/index.ts
+++ server/test/daily/index.ts
@@ -1,7 +1,9 @@
+import { testEndpoints } from "./endpoints.ts";
 import { testIndexGui } from "./index-gui.ts";
 import { testHeatmapGui } from "./heatmap-gui.ts";
 import { testRecordingsGui } from "./recordings-gui.ts";
 
+testEndpoints();
 testIndexGui();
 testHeatmapGui();
 testRecordingsGui();
--- server/test/index.ts
+++ server/test/index.ts
@@ -1,9 +1,7 @@
 import { testHeatmap } from "./heatmap.ts";
 import { testFirebase } from "./firebase.ts";
-import { testEndpoints } from "./endpoints.ts";
 import { testCodec } from "./codec.ts";
 
 testHeatmap();
 testFirebase();
-testEndpoints();
 testCodec();
